bec7d00002ac85bcf26a7ad6030e4e25136064dd
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-05-21 05:44:55 +0200

    Make combineFoldl more efficient

--- src/Result/MyExtra.elm
+++ src/Result/MyExtra.elm
@@ -3,14 +3,24 @@ module Result.MyExtra exposing (combineFoldl)
 
 combineFoldl : (a -> b -> Result error b) -> Result error b -> List a -> Result error b
 combineFoldl f init list =
-    List.foldl
-        (\e acc ->
-            case acc of
-                Err _ ->
-                    acc
+    case init of
+        Err e ->
+            Err e
 
-                Ok a ->
-                    f e a
-        )
-        init
-        list
+        Ok i ->
+            let
+                combineFoldlHelper : b -> List a -> Result error b
+                combineFoldlHelper acc tail =
+                    case tail of
+                        [] ->
+                            Ok acc
+
+                        x :: xs ->
+                            case f x acc of
+                                Err e ->
+                                    Err e
+
+                                Ok y ->
+                                    combineFoldlHelper y xs
+            in
+            combineFoldlHelper i list
