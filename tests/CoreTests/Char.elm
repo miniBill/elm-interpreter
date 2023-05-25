5a30e8c3ab930b85d678f077c7b3eb6212cbec54
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-05-26 00:30:37 +0200

    Char tests

---
+++ tests/CoreTests/Char.elm
@@ -0,0 +1,17 @@
+module CoreTests.Char exposing (suite)
+
+import Test exposing (Test, describe)
+import Utils exposing (evalTest, evalTest_)
+import Value exposing (Value(..))
+
+
+suite : Test
+suite =
+    describe "Char"
+        [ evalTest "Char.toCode UTF-16" "Char.toCode '𝌆'" Int <| Char.toCode '𝌆'
+        , evalTest "Char.fromCode UTF-16" "Char.fromCode 0x0001D306" Char <| Char.fromCode 0x0001D306
+        , evalTest_ "Char.toLocaleLower 'Ì'" Char <| Char.toLocaleLower 'Ì'
+        , evalTest_ "Char.toLocaleLower 'ì'" Char <| Char.toLocaleLower 'ì'
+        , evalTest_ "Char.toLocaleUpper 'Ì'" Char <| Char.toLocaleUpper 'Ì'
+        , evalTest_ "Char.toLocaleUpper 'ì'" Char <| Char.toLocaleUpper 'ì'
+        ]
