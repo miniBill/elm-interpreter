54f76d03713570d0aece678e0e26faef5f535d01
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-06-04 19:45:59 +0200

    Add script with elm-pages

---
+++ script/src/Trace.elm
@@ -0,0 +1,45 @@
+module Trace exposing (run)
+
+import BackendTask exposing (BackendTask)
+import Cli.Option as Option
+import Cli.OptionsParser as OptionsParser
+import Cli.Program as Program
+import Eval
+import Eval.Log as Log
+import FatalError exposing (FatalError)
+import Pages.Script as Script exposing (Script)
+import Rope
+
+
+run : Script
+run =
+    Script.withCliOptions programConfig
+        (\{ input } ->
+            let
+                ( _, _, log ) =
+                    Eval.trace input
+            in
+            log
+                |> Rope.toList
+                |> List.map logLine
+                |> BackendTask.combine
+                |> BackendTask.map (\_ -> ())
+        )
+
+
+logLine : Log.Line -> BackendTask FatalError value
+logLine line =
+    Script.log line.message
+
+
+programConfig : Program.Config { input : String }
+programConfig =
+    Program.config
+        |> Program.add
+            (OptionsParser.build (\input -> { input = input })
+                |> OptionsParser.with (Option.requiredKeywordArg "input")
+            )
+
+
+
+-- Array.toList (Array.initialize 1056 identity)
