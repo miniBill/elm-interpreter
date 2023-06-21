c6d752a9a5baa3818108b8b5d95d78fc5bda0175
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-06-21 17:22:59 +0200

    Tuple tests

---
+++ tests/CoreTests/Tuple.elm
@@ -0,0 +1,35 @@
+module CoreTests.Tuple exposing (suite)
+
+import Test exposing (Test, describe)
+import TestUtils exposing (evalTest, tuple)
+import Value exposing (Value(..))
+
+
+suite : Test
+suite =
+    describe "Tuple Tests"
+        [ describe "first"
+            [ evalTest "extracts first element"
+                "Tuple.first ( 1, 2 )"
+                Int
+                (Tuple.first ( 1, 2 ))
+            ]
+        , describe "second"
+            [ evalTest "extracts second element"
+                "Tuple.second ( 1, 2 )"
+                Int
+                (Tuple.second ( 1, 2 ))
+            ]
+        , describe "mapFirst"
+            [ evalTest "applies function to first element"
+                "Tuple.mapFirst ((*) 5) ( 1, 1 )"
+                (tuple Int Int)
+                (Tuple.mapFirst ((*) 5) ( 1, 1 ))
+            ]
+        , describe "mapSecond"
+            [ evalTest "applies function to second element"
+                "Tuple.mapSecond ((*) 5) ( 1, 1 )"
+                (tuple Int Int)
+                (Tuple.mapSecond ((*) 5) ( 1, 1 ))
+            ]
+        ]
